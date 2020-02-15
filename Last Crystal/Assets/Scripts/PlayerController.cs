ecc515cd2a85e2ab507fa08071247d652f3714b8
Sujay Konda <kondasujay2@gmail.com>
2020-02-15 14:29:52 -0500

    Finished Player Movement

--- Last Crystal/Assets/Scripts/PlayerController.cs
+++ Last Crystal/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     public SpriteRenderer spriteRenderer;
     public Sprite[] sprites;
+    public float Speed;
     void Start()
     {
         
@@ -15,20 +16,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow)){
+        Vector3 translation = new Vector3(0, 0, 0);
+        if(Input.GetKey(KeyCode.UpArrow)){
             spriteRenderer.sprite = sprites[0];
+            translation += Vector3.up * Speed; 
         }
         
-        if(Input.GetKeyDown(KeyCode.DownArrow)){
+        if(Input.GetKey(KeyCode.DownArrow)){
             spriteRenderer.sprite = sprites[2];
+            translation += Vector3.down * Speed;
         }
         
-        if(Input.GetKeyDown(KeyCode.RightArrow)){
+        if(Input.GetKey(KeyCode.RightArrow)){
             spriteRenderer.sprite = sprites[1];
+            translation += Vector3.right * Speed;
         }
         
-        if(Input.GetKeyDown(KeyCode.LeftArrow)){
+        if(Input.GetKey(KeyCode.LeftArrow)){
             spriteRenderer.sprite = sprites[3];
+            translation += Vector3.left * Speed;
         }
+        transform.Translate(translation);
     }
 }
