eb75236412135729c1fb609cb5dbfb33449c8f40
Sujay Konda <kondasujay2@gmail.com>
2020-02-15 15:33:14 -0500

    Fixed Movement

--- Last Crystal/Assets/Scripts/PlayerController.cs
+++ Last Crystal/Assets/Scripts/PlayerController.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public SpriteRenderer spriteRenderer;
-    public Sprite[] sprites;
+    public Animator anim;
     public float Speed;
     void Start()
     {
@@ -14,26 +13,26 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector3 translation = new Vector3(0, 0, 0);
         if(Input.GetKey(KeyCode.UpArrow)){
-            spriteRenderer.sprite = sprites[0];
+            anim.SetInteger("Direction", 0);
             translation += Vector3.up * Speed; 
         }
         
         if(Input.GetKey(KeyCode.DownArrow)){
-            spriteRenderer.sprite = sprites[2];
+            anim.SetInteger("Direction", 3);
             translation += Vector3.down * Speed;
         }
         
         if(Input.GetKey(KeyCode.RightArrow)){
-            spriteRenderer.sprite = sprites[1];
+            anim.SetInteger("Direction", 1);
             translation += Vector3.right * Speed;
         }
         
         if(Input.GetKey(KeyCode.LeftArrow)){
-            spriteRenderer.sprite = sprites[3];
+            anim.SetInteger("Direction", 2);
             translation += Vector3.left * Speed;
         }
         transform.Translate(translation);
