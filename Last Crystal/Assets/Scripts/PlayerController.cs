700bae9273cfa06c088f76a8292cc70a0fa4d023
Sujay Konda <kondasujay2@gmail.com>
2020-02-15 13:35:37 -0500

    Added Anims

--- Last Crystal/Assets/Scripts/PlayerController.cs
+++ Last Crystal/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public Animator anim;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -13,6 +14,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        anim.SetBool("Up", Input.GetKeyDown(KeyCode.Up));
+        anim.SetBool("Down", Input.GetKeyDown(KeyCode.Down));
+        anim.SetBool("Left", Input.GetKeyDown(KeyCode.Left));
+        anim.SetBool("Right", Input.GetKeyDown(KeyCode.Right));
     }
 }
