0f9a07934ac434b2473faf66dc810a34dae3ac79
TheRandomestGuy <arjun.mehan1@gmail.com>
2020-02-15 19:07:01 -0500

    here

--- Last Crystal/Assets/Scripts/HealthPlayer.cs
+++ Last Crystal/Assets/Scripts/HealthPlayer.cs
@@ -8,6 +8,14 @@ public class HealthPlayer : MonoBehaviour
     public SpriteRenderer thing;
     public float helth = 100;
 
+void Start()
+    {
+        for(var i = 0; i < 33; i++){
+            if((i*100/33) < helth && helth <= ((i+1)*100/33)){
+                thing.sprite = Health[i];
+            }
+        }
+    }
     void Update()
     {
         for(var i = 0; i < 33; i++){
