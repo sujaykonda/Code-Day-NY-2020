4c99c13832ef09ec8171fed937aee72fec924646
TheRandomestGuy <arjun.mehan1@gmail.com>
2020-02-16 04:22:20 -0500

    cutscene

---
+++ Last Crystal/Assets/Scripts/AfterAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AfterAnim : MonoBehaviour
+{
+    float timer;
+    public string NextScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > 10f){
+             SceneManager.LoadScene(sceneName:NextScene);
+        }
+    }
+}
