8f7138082e69736d2c0dae6d227b9863b494d66a
TheRandomestGuy <arjun.mehan1@gmail.com>
2020-02-16 07:21:37 -0500

    e

--- Last Crystal/Assets/Scripts/Ending.cs
+++ Last Crystal/Assets/Scripts/Ending.cs
@@ -3,24 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class AfterAnim : MonoBehaviour
+public class Ending : MonoBehaviour
 {
-    float timer;
-    public string NextScene;
     public string LastScene;
-
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        if(timer > 10f){
-             SceneManager.LoadScene(sceneName:NextScene);
+        if(PrismsDefeated.defeatedPrisms.ToArray().Length >= 4){
+            SceneManager.LoadScene(sceneName:LastScene);
         }
     }
 }
