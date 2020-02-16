a44c3499e4b8ba57f87ae151741a0ee273099ede
Sujay Konda <kondasujay2@gmail.com>
2020-02-16 06:38:21 -0500

    Added Prismslkwakf

---
+++ Last Crystal/Assets/Scripts/PrismsDefeated.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PrismsDefeated{
+    public static List<string> defeatedPrisms = new List<string>();
+}
