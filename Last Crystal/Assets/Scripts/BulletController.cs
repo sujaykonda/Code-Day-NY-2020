4f946d0033f27ce73ce1453323758e52b4992a1c
Sujay Konda <kondasujay2@gmail.com>
2020-02-16 02:34:34 -0500

    l

--- Last Crystal/Assets/Scripts/BulletController.cs
+++ Last Crystal/Assets/Scripts/BulletController.cs
@@ -8,16 +8,26 @@ public class BulletController : MonoBehaviour
     public float speed = 0.1f;
     public float aiming = 0f;
     public Transform tar;
+    public float time = 5f;
+    float timer = 0;
     void Start(){
         tar = GameObject.Find("Player").transform;
     }
     // Update is called once per frame
     void FixedUpdate()
     {
+        timer += Time.fixedDeltaTime;
         Vector3 diff = transform.position - tar.position;
-        float angle = ((Mathf.Atan2(diff.y, diff.x)*Mathf.Rad2Deg - transform.eulerAngles.z + 90f)%360)*aiming;
-        transform.eulerAngles = new Vector3(0f,0f, (angle + transform.eulerAngles.z)%360);
+        float diffAngle = (Mathf.Atan2(diff.y, diff.x)*Mathf.Rad2Deg - transform.eulerAngles.z + 90f);
+        if(Mathf.Abs(diffAngle) > 180){
+            diffAngle = -Mathf.Abs(diffAngle)/diffAngle*(360-Mathf.Abs(diffAngle));
+        }
+        float angle = diffAngle*aiming;
+        transform.eulerAngles = new Vector3(0f,0f, (angle + transform.eulerAngles.z));
         transform.Translate((Vector3.up * speed));
+        if(timer > time){
+            Destroy(gameObject);
+        }
     }
     void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "good"){
